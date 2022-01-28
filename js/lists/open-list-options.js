1a4bdd8e8a42d04537d29b254ba4e99cd8e52e63
DevRasta07 <77172676+DevRasta07@users.noreply.github.com>
2022-01-27 22:11:16 -0300

    Add files via upload

--- js/lists/open-list-options.js
+++ js/lists/open-list-options.js
@@ -1,3 +1,50 @@
 function openListOptions(btn) {
-    console.log(btn)
+    const coords = getPos(btn)
+    const {x, y} = coords
+    
+    btn.blur()
+    console.log(coords)
+    createListMenu()
+
+    function createListMenu() {
+        let menuOverlay = document.createElement("div")
+        menuOverlay.classList.add("menu-overlay")
+        document.body.appendChild(menuOverlay)
+
+        let listMenuContainer = document.createElement("div")
+        listMenuContainer.classList.add("list-menu-container")
+        menuOverlay.appendChild(listMenuContainer)
+        listMenuContainer.style.left = `${x}px`
+        listMenuContainer.style.top = `${y}px`
+
+        let option = document.createElement("div")
+        option.classList.add("option")
+         
+        let optionIcon = document.createElement("i")
+        option.appendChild(optionIcon)
+
+        let optionText = document.createElement("span")
+        option.appendChild(optionText)
+
+        let deleteOption = option.cloneNode(true)
+        let deleteIcon = deleteOption.querySelector("i")
+        let deleteText = deleteOption.querySelector("span")
+        deleteIcon.classList.add("fas", "fa-trash")
+        deleteText.innerText = "Delete"
+        listMenuContainer.appendChild(deleteOption)
+
+        let renameOption = option.cloneNode(true)
+        let renameIcon = renameOption.querySelector("i")
+        let renameText = renameOption.querySelector("span")
+        renameIcon.classList.add("fas", "fa-pen")
+        renameText.innerText = "Rename"
+        listMenuContainer.appendChild(renameOption)
+
+        console.log(menuOverlay)
+    }
+
+    function getPos(elm) {
+        let rect = elm.getBoundingClientRect()
+        return { x:rect.left, y:rect.top }
+    }
 }
